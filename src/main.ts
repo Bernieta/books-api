2f76b6eaae7ff5083ae6ae050bc96fda76fa53ff
Juan Camilo Arrieta Bernal <arrietacamilo530@gmail.com>
2024-06-10 09:36:11 -0500

    chore: changed global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api');
   app.enableCors({
     origin: '*',
   });
